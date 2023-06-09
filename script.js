996d04574d3542c6574343f40559df43dd836544
YiftachDev <yiftach007@gmail.com>
2023-06-09 11:33:32 +0300

    Handeled submit form

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook);
 }
 
 const newBookBtn = document.querySelector("#new-book-btn");
@@ -16,4 +21,11 @@ const newBookBtn = document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click", () => {
   let form = document.querySelector("#new-book-form");
   form.style.display = "block";
+});
+
+const form = document.querySelector("#new-book-form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  addBookToLibrary();
 });
