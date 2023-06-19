002876499b744e2e841ad26ea63789b2755b0551
YiftachDev <yiftach007@gmail.com>
2023-06-19 13:06:39 +0300

    Created class Book

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
-
-Book.prototype.toggleRead = function () {
-  this.read = !this.read;
-};
 
 function toggleRead(index) {
   myLibrary[index].toggleRead();
